e92bc04930e8123c7f313c068825090bb1f5aa34
Icarus <thefalloficarus19@gmail.com>
2024-09-15 19:35:18 +0530

    learning c++ basic io

---
+++ Basics/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> //allows to take input and produce output
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello World" << "\n";
+    std::cout << "This is Vedanshee" << std:: endl; // backslash and endl do the same thing but usually backslash is used because it is faster
+
+    //taking an input
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x:" << x << " and y" << y;
+    return 0;
+}

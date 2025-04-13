18c85c45e61ca7a8b75fb1357b1f95858f3c3409
Icarus <thefalloficarus19@gmail.com>
2025-04-13 15:43:04 +0530

    apnacollege hw questions added

--- Basics/functions.cpp
+++ Basics/functions.cpp
@@ -10,8 +10,39 @@ int sum(int a, int b){
     int sum = a+b;
     return sum;
 }
-int main(){
 
+bool isPrime(int n){
+    if (n<=1){
+        return false;
+    }
+    else{
+        for(int i=2; i*i<=n; i++){
+            if(n%i == 0) return false;
+        }
+    }
+    return true;
+}
+
+void printPrime(int n){
+    for(int i=2; i<=n; i++){
+        if(isPrime(i)){
+            cout << i << " ";
+    }
+    }
+}
+
+int fibo(int n){
+    if(n<=1) return n;
+    int a=0, b=1, next;
+    for(int i=2; i<=n; i++){
+        next=a+b;
+        a=b;
+        b=next;
+    }
+    return b;
+}
+
+int main(){
     /*
     string name;
     cin >> name;
@@ -21,13 +52,18 @@ int main(){
     cin >> a >>b;
     ans = sum(a,b);
     cout << ans;
-    */
+    
     
     //INBUILT FUNCTIONS
     int num1, num2;
     cin >> num1 >> num2;
     int maximum = max(num1, num2);
     cout << maximum;
-    
+    */
+
+    cout << "The number is prime: " << isPrime(47) < endl;
+    printPrime(30);
+    cout << endl;
+    cout << "10th number in the fibonacci series is: " << fibo(10) << endl;
     return 0;
 }
